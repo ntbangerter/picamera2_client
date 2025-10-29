1b31ebb45cb9280350763a0935f7741c74978b02
Tanner Bangerter <tanner.bangerter@gmail.com>
2025-10-29 18:14:59 -0400

    remove transformers logic

--- client.py
+++ client.py
@@ -64,25 +64,6 @@ def dummy(img):
     return img
 
 
-# import transformers
-
-# def run_image_classification():
-#     processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224', device_map='cuda')
-#     model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224', device_map='cuda')
-
-#     def image_classification(img):
-#         inputs = processor(images=img, return_tensors='pt').to('cuda')
-#         outputs = model(**inputs)
-#         logits = outputs.logits
-#         predicted_class_idx = logits.argmax(-1).item()
-#         print("Predicted class:", model.config.id2label[predicted_class_idx])
-
-#     mjpeg_stream_watcher(
-#         fn=image_classification,
-#         imshow=True,
-#     )
-    
-
 if __name__ == "__main__":
     mjpeg_stream_watcher(
         fn=dummy,
